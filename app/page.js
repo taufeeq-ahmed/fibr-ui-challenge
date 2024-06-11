bdacb65a65e2b9bffdd1142be8dff0797a721c0b
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-06-11 22:19:11 +0530

    new changes

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+"use client"
+
 import styles from "./page.module.css";
 import Header from "@/components/Header";
 import BasicDetails from "@/page_components/BasicDetails";
--- page_components/BasicDetails/index.js
+++ page_components/BasicDetails/index.js
@@ -1,37 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./styles.module.css"
 import { Space_Grotesk } from "next/font/google";
 import Image from 'next/image';
 import markerIcon from '@/public/icons/marker.svg'
+import TextInput from '@/components/Input';
 
 const groteskFont = Space_Grotesk({ subsets: ["latin"] });
 
 function BasicDetails({
     title, description
 }) {
+    const [mode, setMode] = useState("edit")
+
     return (
         <div className={styles.basic_details}>
-            <div className={styles.display}>
-                <h3
-                    className={
-                        `${groteskFont.className} ${styles.title}`
-                    }
-                >
-                    {title}
-                </h3>
-                <p className={styles.description}>{description}</p>
-            </div>
-            <div className={styles.edit_control}>
-                <span className={styles.edit_icon_container}>
-                    <Image
-                        className={styles.edit_icon}
-                        src={markerIcon}
-                        width={13}
-                        height={13}
-                    />
-                </span>
-                <span>Edit</span>
-            </div>
+            {mode === "display" ? (
+                <>
+                    <div className={styles.display}>
+                        <h3
+                            className={
+                                `${groteskFont.className} ${styles.title}`
+                            }
+                        >
+                            {title}
+                        </h3>
+                        <p className={styles.description}>{description}</p>
+                    </div>
+                    <div
+                        className={styles.edit_control}
+                        onClick={() => { setMode("edit") }}
+                    >
+                        <span className={styles.edit_icon_container}>
+                            <Image
+                                className={styles.edit_icon}
+                                src={markerIcon}
+                                width={13}
+                                height={13}
+                            />
+                        </span>
+                        <span>Edit</span>
+                    </div>
+                </>
+            ) : (
+                <form className={styles.basic_details_form}>
+
+                </form>
+            )}
         </div>
     )
 }

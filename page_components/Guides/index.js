7086a1127fc08330b79efed19a9201e10dce2a2b
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-06-13 13:53:49 +0530

    guides comp created

---
+++ page_components/Guides/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./styles.module.css"
+
+function Guides() {
+    return (
+        <div className={styles.guides}>
+            Guides
+        </div>
+    )
+}
+
+export default Guides
